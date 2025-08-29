3e930a21ef5842397091a259f8bd94eeb67bde0d
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-29 11:30:19 -0500

    notion: assets & code

---
+++ app/src/main/java/com/tineo/cik/ui/Streak.java
@@ -0,0 +1,43 @@
+package com.tineo.cik.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tineo.cik.R;
+import com.tineo.cik.databinding.FragmentStreakBinding;
+
+public class Streak extends Fragment {
+    FragmentStreakBinding binding;
+    View view;
+    Context context;
+    NavController navController;
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentStreakBinding.inflate(inflater, container, false);
+        return view = binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        navController = Navigation.findNavController(view);
+    }
+}
